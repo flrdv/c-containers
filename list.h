d36d89f4a7c1f4a68d9e5cc40fba46f40c10e3b9
Pavlo <real.floordiv@gmail.com>
2025-03-10 10:03:45 +0100

    add full-blown dynarray

---
+++ list.h
@@ -0,0 +1,51 @@
+//
+// Created by pavlo on 10.01.25.
+//
+
+#pragma once
+
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+
+// List initializes a new list-type with the provided type as an anonymous structure.
+// Therefore, it may be a good idea to typedef it.
+#define List(T) struct { size_t len; size_t cap; T* ptr; }
+
+// NEWLIST is an initializer of an empty list of type T and capable of containing `size`
+// elements before reallocations.
+#define NEWLIST(T, size) { .len = 0, .cap = size, .ptr = malloc(size*sizeof(T)) }
+
+// PUSH appends a new value to the end of a list and reallocates if new space is needed.
+// Cannot be used as an expression.
+#define PUSH(list, elem)                  \
+{                                         \
+    if (list.len >= list.cap) GROW(list); \
+    list.ptr[list.len++] = elem;          \
+}
+
+// POP removes the last element in a list and returns it back. Can be used as an expression.
+#define POP(list) (list.ptr[--list.len])
+
+// GROW increases the list's capacity further. Intended for internal use by PUSH primarily.
+// Cannot be used as an expression.
+//
+// Note: if realloc fails, an assertation is raised. However, if NDEBUG is defined, list.ptr
+// will be silently be set to NULL, therefore next list access most likely results in segfault.
+#define GROW(list)                                                 \
+{                                                                  \
+    const size_t threshold = 256;                                  \
+    size_t nextcap;                                                \
+    if (list.cap < threshold)                                      \
+        nextcap = (list.cap|1)*2;                                  \
+    else                                                           \
+        nextcap = list.cap + ((list.cap + 3*threshold) >> 2);      \
+    void* newptr = realloc(list.ptr, nextcap*sizeof(list.ptr[0])); \
+    assert(list.cap < nextcap /*next capacity overflow*/);         \
+    assert(newptr != NULL /*realloc failed*/);                     \
+    list.ptr = newptr;                                             \
+    list.cap = nextcap;                                            \
+}
+
+// FREE deallocates a list by freeing its pointer. Can be used as an expression.
+#define FREE(list) free(list.ptr)
---
+++ tests/list_test.c
@@ -0,0 +1,61 @@
+//
+// Created by Paul on 10.03.25.
+//
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "unity.h"
+#include "list.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+typedef List(uint32_t) ListU32;
+
+ListU32 fill_list(ListU32 list) {
+    for (uint32_t i = 0; i < 10; i++)
+        PUSH(list, i);
+    return list;
+}
+
+void test_append_(ListU32 list) {
+    list = fill_list(list);
+
+    TEST_ASSERT(list.len == 10);
+    TEST_ASSERT(list.len <= list.cap);
+    TEST_ASSERT_NOT_NULL(list.ptr);
+
+    for (ssize_t i = 0; i < 10; i++)
+        TEST_ASSERT(list.ptr[i] == i);
+}
+
+void test_push_to_empty_list() {
+    ListU32 list = {0};
+    test_append_(list);
+    FREE(list);
+}
+
+void test_push_to_preallocd_list() {
+    ListU32 list = NEWLIST(uint32_t, 10);
+    uint32_t* origptr = list.ptr;
+    test_append_(list);
+    TEST_ASSERT(origptr == list.ptr);
+    FREE(list);
+}
+
+void test_pop() {
+    ListU32 list = fill_list((ListU32) NEWLIST(uint32_t, 10));
+    uint32_t counter = 9;
+    while (list.len > 0)
+        TEST_ASSERT(POP(list) == counter--);
+    FREE(list);
+}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_push_to_empty_list);
+    RUN_TEST(test_push_to_preallocd_list);
+    RUN_TEST(test_pop);
+    return UNITY_END();
+}
